9cec76d797ebc9d87c58902dffbc80962a4415de
Halil İbrahim Şenaydın <70847361+halilsenaydin@users.noreply.github.com>
2024-05-24 12:20:05 +0300

    swagger

--- swagger.js
+++ swagger.js
@@ -4,7 +4,7 @@ const options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'Eteration Case',
+            title: 'Eteration Node.js Case',
             description: 'Eteration Case API Documentation',
             version: '1.0.0',
         },
